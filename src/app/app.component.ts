19aa9cac49b4ddb561cfd13590f1c58a9d0de467
Viacheslav-ua <atph@ukr.net>
2022-09-21 09:26:48 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'angular-pipe-lessons'
-
-  transform(value: string, prefix: string, operator: 'uppercase' | 'lowercase' = 'uppercase'): string {
-    console.log('AppComponent', value)
-    value = !value ? prefix : prefix + ' ' + value
-
-    switch (operator) {
-      case 'uppercase':
-        return value.toUpperCase()
-
-      default:
-        return value.toLowerCase()
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextTransformPipe } from './pipes/text-transform.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextTransformPipe,
   ],
   imports: [
     BrowserModule,

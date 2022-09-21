b5f7ebe00c9e51aec3826d55a9754d62e5156204
Viacheslav-ua <atph@ukr.net>
2022-09-21 08:51:48 +0300

    lesson-#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'angular-pipe-lessons';
+  title = 'angular-pipe-lessons'
+
+  transform(value: string, prefix: string, operator: 'uppercase' | 'lowercase' = 'uppercase'): string {
+    console.log('AppComponent', value)
+    value = !value ? prefix : prefix + ' ' + value
+
+    switch (operator) {
+      case 'uppercase':
+        return value.toUpperCase()
+
+      default:
+        return value.toLowerCase()
+    }
+  }
 }
